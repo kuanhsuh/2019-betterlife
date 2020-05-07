9413fb155b603dcb080f1983340cf3655f491fce
dannyhuang <kuanhsuh@gmail.com>
2020-05-07 14:17:23 +0800

    fix logo css

--- header.php
+++ header.php
@@ -54,7 +54,8 @@
     <div class="container mx-auto md:px-0">
       <div class="flex flex-wrap justify-between items-center md:items-end">
         <a href="/">
-          <img src="<?php echo get_theme_file_uri('/assets/logo.png') ?>" </a> <ul class="hidden md:flex md:items-end md:w-auto">
+          <img src="<?php echo get_theme_file_uri('/assets/logo.png') ?>" class="h-10" /> </a>
+        <ul class="hidden md:flex md:items-end md:w-auto ">
           <li class="mr-2 px-2 <?php if (is_page(2)) echo 'text-gray-600 font-medium'; ?>">
             <a href="<?php echo esc_html(site_url("/")) ?>">首頁</a>
           </li>
@@ -63,26 +64,26 @@
           <li class="mr-2 px-2 <?php if (is_page(21)) echo 'text-gray-600 font-medium'; ?>"><a href="<?php echo esc_html(site_url("/inquiry")) ?>">我要詢價</a></li>
           <li class="mr-2 px-2 <?php if (is_page(18)) echo 'text-gray-600 font-medium'; ?>"><a href="<?php echo esc_html(site_url("/all-articles")) ?>">相關文章</a></li>
           <li class="pl-2 <?php if (is_page(24)) echo 'text-gray-600 font-medium'; ?>"><a href="<?php echo esc_html(site_url("/contact")) ?>">聯絡我們</a></li>
-          </ul>
-          <span onclick="toggleMenu()" class="block md:hidden">
-            <div class="nav-bar"></div>
-            <div class="nav-bar"></div>
-            <div class="nav-bar"></div>
-          </span>
-          <ul id="responsive" class="sidebar hidden w-1/2 shadow h-full text-center md:hidden bg-white fixed" style="z-index: 50; top: 0; right: 0">
-            <li class="py-3 text-right pr-6">
-              <span onclick="toggleMenu()" class="inline-block relative" style="width: 25px; height: 25px; margin-top: 15px;">
-                <div class="nav-cross absolute"></div>
-                <div class="nav-cross-2 absolute top-0"></div>
-              </span>
-            </li>
-            <li class="py-2 text-gray-600"><a href="#">首頁</a></li>
-            <li class="py-2"><a href="<?php echo esc_html(site_url("/company")) ?>">公司介紹</a></li>
-            <li class="py-2"><a href="<?php echo esc_html(site_url("/products")) ?>">所有產品</a></li>
-            <li class="py-2"><a href="<?php echo esc_html(site_url("/inquiry")) ?>">我要詢價</a></li>
-            <li class="py-2"><a href="<?php echo esc_html(site_url("/all-articles")) ?>">相關文章</a></li>
-            <li class="py-2"><a href="<?php echo esc_html(site_url("/contact")) ?>">聯絡我們</a></li>
-          </ul>
+        </ul>
+        <span onclick="toggleMenu()" class="block md:hidden">
+          <div class="nav-bar"></div>
+          <div class="nav-bar"></div>
+          <div class="nav-bar"></div>
+        </span>
+        <ul id="responsive" class="sidebar hidden w-1/2 shadow h-full text-center md:hidden bg-white fixed" style="z-index: 50; top: 0; right: 0">
+          <li class="py-3 text-right pr-6">
+            <span onclick="toggleMenu()" class="inline-block relative" style="width: 25px; height: 25px; margin-top: 15px;">
+              <div class="nav-cross absolute"></div>
+              <div class="nav-cross-2 absolute top-0"></div>
+            </span>
+          </li>
+          <li class="py-2 text-gray-600"><a href="#">首頁</a></li>
+          <li class="py-2"><a href="<?php echo esc_html(site_url("/company")) ?>">公司介紹</a></li>
+          <li class="py-2"><a href="<?php echo esc_html(site_url("/products")) ?>">所有產品</a></li>
+          <li class="py-2"><a href="<?php echo esc_html(site_url("/inquiry")) ?>">我要詢價</a></li>
+          <li class="py-2"><a href="<?php echo esc_html(site_url("/all-articles")) ?>">相關文章</a></li>
+          <li class="py-2"><a href="<?php echo esc_html(site_url("/contact")) ?>">聯絡我們</a></li>
+        </ul>
       </div>
     </div>
   </nav>
