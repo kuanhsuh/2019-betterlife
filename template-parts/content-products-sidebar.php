99c4fede301927eb2897689cadf18777019c24e9
dannyhuang <kuanhsuh@gmail.com>
2020-10-15 09:38:16 +0800

    Fix Product Sidebar link

--- template-parts/content-products-sidebar.php
+++ template-parts/content-products-sidebar.php
@@ -37,7 +37,14 @@
         </svg>
       </a>
     </li>
-    <li class="p-4 <?php if (is_category(9)) echo 'text-teal-800 font-medium'; ?>"><a href="<?php echo esc_html(site_url("/category/product-heat-shield")) ?>" class="flex items-end">貼膜<span class="italic text-xs mr-auto ml-2">(Screeb)</span>
+    <li class="p-4 <?php if (is_category(9)) echo 'text-teal-800 font-medium'; ?>"><a href="<?php echo esc_html(site_url("/category/product-heat-shield")) ?>" class="flex items-end">防曬隔熱產品<span class="italic text-xs mr-auto ml-2">(Film and Glass)</span>
+        <svg class="svg-icon" viewBox="0 0 20 20">
+          <path d="M11.611,10.049l-4.76-4.873c-0.303-0.31-0.297-0.804,0.012-1.105c0.309-0.304,0.803-0.293,1.105,0.012l5.306,5.433c0.304,0.31,0.296,0.805-0.012,1.105L7.83,15.928c-0.152,0.148-0.35,0.223-0.547,0.223c-0.203,0-0.406-0.08-0.559-0.236c-0.303-0.309-0.295-0.803,0.012-1.104L11.611,10.049z">
+          </path>
+        </svg>
+      </a>
+    </li>
+    <li class="p-4 <?php if (is_category(9)) echo 'text-teal-800 font-medium'; ?>"><a href="<?php echo esc_html(site_url("/category/product-screen")) ?>" class="flex items-end">機能紗網<span class="italic text-xs mr-auto ml-2">(Screen)</span>
         <svg class="svg-icon" viewBox="0 0 20 20">
           <path d="M11.611,10.049l-4.76-4.873c-0.303-0.31-0.297-0.804,0.012-1.105c0.309-0.304,0.803-0.293,1.105,0.012l5.306,5.433c0.304,0.31,0.296,0.805-0.012,1.105L7.83,15.928c-0.152,0.148-0.35,0.223-0.547,0.223c-0.203,0-0.406-0.08-0.559-0.236c-0.303-0.309-0.295-0.803,0.012-1.104L11.611,10.049z">
           </path>
