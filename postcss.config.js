3eed35d7cd8ede720a9b8426995e714f0f354419
dannyhuang <kuanhsuh@gmail.com>
2020-11-04 14:50:45 +0800

    Update Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,17 @@
 const purgecss = require("@fullhuman/postcss-purgecss");
 const autoprefixer = require("autoprefixer");
+const cssnano = require('cssnano')
 
 module.exports = {
   plugins: [
     require("tailwindcss"),
-    // purgecss({
-    //   content: ["./**/*.php"],
-    //   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    // }),
+    purgecss({
+      content: ["./**/*.php"],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    }),
+    cssnano({
+      preset: 'default'
+    }),
     autoprefixer()
   ]
 };
